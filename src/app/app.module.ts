2edc882103ba016be86a2284540d8f967638c804
Raúl Correia <1090657@isep.ipp.pt>
2018-11-17 20:59:52 +0000

    changed size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import {AppComponent} from './app.component';
-import {ConfigService} from './config.service';
-import {SceneComponent} from './scene.component';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {NgcFloatButtonModule} from 'ngc-float-button';
-import { FloatingButtonComponent } from './floating-button/floating-button.component'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppComponent } from './app.component';
+import { ConfigService } from './config.service';
+import { SceneComponent } from './scene.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { NgcFloatButtonModule } from 'ngc-float-button';
+import { FloatingButtonComponent } from './floating-button/floating-button.component';
+import { ObjectControlComponent } from './object-control/object-control.component'
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   imports: [
     BrowserModule,
@@ -18,7 +23,11 @@ import { FloatingButtonComponent } from './floating-button/floating-button.compo
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    NgcFloatButtonModule
+    NgcFloatButtonModule,
+    MatSliderModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     ConfigService,
@@ -27,9 +36,10 @@ import { FloatingButtonComponent } from './floating-button/floating-button.compo
     AppComponent,
     SceneComponent,
     FloatingButtonComponent,
+    ObjectControlComponent,
   ],
   bootstrap: [
     AppComponent,
   ],
 })
-export class AppModule {}
+export class AppModule { }
