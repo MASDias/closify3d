78a0a3caec95c812b85a413d1e047f58ef7c4769
Miguel Santos [1161386] <1161386@isep.ipp.pt>
2018-11-17 16:51:15 +0000

    Divisao visivel/invisivel

---
+++ src/app/model/divisao.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+export class Divisao extends THREE.Group {
+    constructor(altura, profundidade, invisivel) {
+
+        super();
+
+        this.espessura = 1;
+
+        var divisionGeometry;
+        var divisionMaterial;
+        // Divisao
+        if (invisivel) {
+            divisionGeometry = new THREE.PlaneGeometry(profundidade - this.espessura, altura - (this.espessura * 2));
+            divisionMaterial = new THREE.MeshLambertMaterial({
+                map: new THREE.TextureLoader().load('assets/texture/wood3.jpg'),
+                transparent: true,
+                opacity: 0.3,
+                side: THREE.DoubleSide
+            });
+        } else {
+            divisionGeometry = new THREE.BoxGeometry(this.espessura, altura - (this.espessura * 2), profundidade - this.espessura);
+            divisionMaterial = new THREE.MeshLambertMaterial({
+                map: new THREE.TextureLoader().load('assets/texture/wood3.jpg'),
+                side: THREE.DoubleSide
+            });
+        }
+
+        var divisao = new THREE.Mesh(divisionGeometry, divisionMaterial);
+        divisao.castShadow = false;
+        if(invisivel) divisao.rotateY(3.1415 / 2);
+        divisao.position.z = this.espessura / 2;
+
+
+        // adding to the group
+        this.add(divisao);
+
+    }
+
+}
