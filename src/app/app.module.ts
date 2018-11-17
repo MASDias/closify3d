79d486e4bb90c268796f8a34be5936fc78853e7b
Raúl Correia <1090657@isep.ipp.pt>
2018-11-17 17:00:39 +0000

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,21 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {AppComponent} from './app.component';
 import {ConfigService} from './config.service';
 import {SceneComponent} from './scene.component';
-
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {NgcFloatButtonModule} from 'ngc-float-button';
+import { FloatingButtonComponent } from './floating-button/floating-button.component'
 @NgModule({
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    NgcFloatButtonModule
   ],
   providers: [
     ConfigService,
@@ -17,6 +26,7 @@ import {SceneComponent} from './scene.component';
   declarations: [
     AppComponent,
     SceneComponent,
+    FloatingButtonComponent,
   ],
   bootstrap: [
     AppComponent,
